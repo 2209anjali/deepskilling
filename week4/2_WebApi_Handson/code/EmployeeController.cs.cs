dcb8c964c30161148b52aae003ca5197dca40474
anjali <168843438+2209anjali@users.noreply.github.com>
2025-07-13 23:33:14 +0545

    Add files via upload

---
+++ week4/2_WebApi_Handson/code/EmployeeController.cs.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace mynewapi.Controllers
+{
+    [ApiController]
+    [Route("api/emp")]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var employees = new List<string> { "Jonny", "Jahan", "Alia" };
+            return Ok(employees);
+        }
+    }
+}
